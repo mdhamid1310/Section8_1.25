963cf3cf4eb7cf071e8751f55c10e947b2b7dfd7
mdhamid1310 <john@example.com>
2023-10-27 10:01:50 +0530

    Add project files.

---
+++ Section8_1.25/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section8_1._25
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            runApp();
+            Console.ReadLine();
+        }
+        public static void runApp()
+        {
+            string[] students = { "Alice", "Bob", "Charlie", "David", "Ella", "Frank", "Grace", "Henry", "Ivy", "Jack" };
+
+            Console.WriteLine("Students (Recursion):");
+            printViaRecursion(students, 0, students.Length);
+        }
+
+        public static void printViaRecursion(string[] arr, int index, int length)
+        {
+            if (index < length)
+            {
+                Console.WriteLine(arr[index]);
+                printViaRecursion(arr, index + 1, length);
+            }
+        }
+
+
+    }
+}
